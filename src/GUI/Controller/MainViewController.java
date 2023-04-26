fd9fac2c5d7b27e490973a01f2554369757c0cc7
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2023-04-26 11:40:48 +0200

    sliding menu

--- src/GUI/Controller/MainViewController.java
+++ src/GUI/Controller/MainViewController.java
@@ -1,13 +1,50 @@
 package GUI.Controller;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 public class MainViewController {
-    public Button createCustomers;
-    public TextField searchBar;
+    @FXML
+    private Button createCustomers;
+    @FXML
+    private TextField searchBar;
+    @FXML
+    private Pane createCustomerMenu;
 
-    public void handleCreateCustomers(ActionEvent actionEvent) {
+
+    public void handleCreateCustomersMenu(ActionEvent actionEvent) {
+        customerMenu();
+    }
+
+    public void handleCreateCustomer(ActionEvent actionEvent) {
+    }
+
+    public void handleCancelCustomer(ActionEvent actionEvent) {
+        customerMenu();
+    }
+
+
+
+    private void customerMenu() {
+        if (createCustomerMenu.isVisible()) {
+            TranslateTransition slideOut = new TranslateTransition(Duration.seconds(0.5), createCustomerMenu);
+            slideOut.setToX(-createCustomerMenu.getWidth());
+            slideOut.setOnFinished(e -> createCustomerMenu.setVisible(false));
+            slideOut.play();
+        } else {
+            createCustomerMenu.setTranslateX(-createCustomerMenu.getWidth());
+            createCustomerMenu.toFront();
+            createCustomerMenu.setVisible(true);
+
+            TranslateTransition slideIn = new TranslateTransition(Duration.seconds(0.5), createCustomerMenu);
+            slideIn.setToX(0);
+            slideIn.play();
+        }
     }
 }
