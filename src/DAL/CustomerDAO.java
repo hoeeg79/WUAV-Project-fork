e93f3b285e172a1911d3b060206953bd4ce14cc8
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2023-04-26 10:11:31 +0200

    Still working on it

--- src/DAL/CustomerDAO.java
+++ src/DAL/CustomerDAO.java
@@ -13,10 +13,19 @@ public class CustomerDAO {
     }
 
     public Customer createCustomer(Customer customer) {
+
+        //Prepare variables from customer in parameter
         String name = customer.getName();
         String email = customer.getEmail();
         int tlf = customer.getTlf();
-        String sql = "INSERT INTO User_credentials (username, password, admin) VALUES (?,?,?);";
+        String picture = "";
+        if (customer.getPicture() == null){
+            picture = "skriv sti til default billede her";
+        } else {
+            picture = customer.getPicture();
+        }
+
+        String sql = "INSERT INTO Customer (name, email, tlf, image) VALUES (?,?,?,?);";
 
         try (Connection conn = dbc.getConnection()) {
             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -24,6 +33,7 @@ public class CustomerDAO {
             stmt.setString(1, name);
             stmt.setString(2, email);
             stmt.setInt(3, tlf);
+            stmt.setString(4, picture);
 
             stmt.executeUpdate();
 
@@ -35,7 +45,7 @@ public class CustomerDAO {
                 id = rs.getInt(1);
             }
 
-            return new User(username, id, isAdmin, password);
+            return new Customer(id, name, email, tlf, picture);
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
