1ba4366bf94e4ed10dc165ce5b03cb3b4903c20d
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-04-26 10:56:46 +0200

    Vi er i modellen

--- src/GUI/Model/CustomerModel.java
+++ src/GUI/Model/CustomerModel.java
@@ -2,15 +2,26 @@ package GUI.Model;
 
 import BE.Customer;
 import BLL.CustomerManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class CustomerModel {
     private CustomerManager customerManager;
 
+    private ObservableList<Customer> customerList;
+
     public CustomerModel() throws Exception {
         customerManager = new CustomerManager();
+        customerList = FXCollections.observableArrayList();
     }
 
     public void createCustomer(Customer customer) {
         createCustomer(customer);
+    }
+
+    public ObservableList<Customer> getCustomerList() throws Exception{
+        customerList.clear();
+        customerList.addAll(customerManager.getCustomers());
+        return customerList;
     }
 }
