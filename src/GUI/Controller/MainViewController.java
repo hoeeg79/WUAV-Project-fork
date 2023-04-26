41b0e349df3e6a07aa333251645d14665f01f28b
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2023-04-26 11:08:11 +0200

    CreateUsersView & Controller

--- src/GUI/Controller/MainViewController.java
+++ src/GUI/Controller/MainViewController.java
@@ -1,13 +1,42 @@
 package GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainViewController {
+    @FXML
     public Button createCustomers;
+    @FXML
     public TextField searchBar;
+    @FXML
+    public Button createUsers;
 
+    @FXML
     public void handleCreateCustomers(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleCreateUsers(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/CreateUsersView.fxml"));
+        Parent root = loader.load();
+
+        //Someday we will put a model here
+
+        stage.setScene(new Scene(root));
+        stage.setTitle("Create User");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+        stage.show();
     }
 }
