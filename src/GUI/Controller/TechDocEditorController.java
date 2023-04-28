1f6f75e6452a8576d9b3bbd691bc1934696b02b4
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2023-04-28 17:48:15 +0200

    j

--- src/GUI/Controller/TechDocEditorController.java
+++ src/GUI/Controller/TechDocEditorController.java
@@ -4,8 +4,16 @@ import BE.TechDoc;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class TechDocEditorController extends BaseController{
+    @FXML
+    private TextArea taDeviceInfo;
+    @FXML
+    private TextArea taSetupDescription;
+    @FXML
+    private TextField tfTitle;
     @FXML
     private Button btnClose;
     private TechDoc techDoc;
@@ -24,7 +32,11 @@ public class TechDocEditorController extends BaseController{
         closeWindow(btnClose);
     }
 
-    public void setTechDoc(TechDoc techDoc) {
+public void setTechDoc(TechDoc techDoc) {
         this.techDoc = techDoc;
+    }
+
+    @FXML
+    private void handleSave(ActionEvent actionEvent) {
     }
 }
