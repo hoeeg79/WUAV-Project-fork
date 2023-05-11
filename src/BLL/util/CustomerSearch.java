0d1db6d4cdd533161bebbab94b93e7d8a98cb5ef
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-05-11 13:00:58 +0200

    search

--- src/BLL/util/CustomerSearch.java
+++ src/BLL/util/CustomerSearch.java
@@ -17,7 +17,7 @@ public class CustomerSearch {
 
         for(Customer customer: search){
 
-            if(compareName(searchQuery, customer) || compareTlf(searchQuery, customer) || compareEmail(searchQuery, customer) || compareZipCode(searchQuery, customer) || compareStreetName(searchQuery, customer)){
+            if(compareName(searchQuery, customer) || compareTlf(searchQuery, customer) || compareEmail(searchQuery, customer) || compareZipCode(searchQuery, customer) || compareStreetName(searchQuery, customer) || compareCity(searchQuery, customer)){
                 matchingCustomers.add(customer);
         }
     }
@@ -37,16 +37,30 @@ public class CustomerSearch {
     private boolean compareTlf(String searchQuery, Customer customer){
         return customer.getTlf().toLowerCase().contains(searchQuery.toLowerCase());
     }
-
+    /**
+     * A method that takes in a search query and a customer, and returns a boolean value indicating whether email matches the search
+     */
     private boolean compareEmail(String searchQuery, Customer customer){
         return customer.getEmail().toLowerCase().contains(searchQuery.toLowerCase());
     }
-
+    /**
+     * A method that takes in a search query and a customer, and returns a boolean value indicating whether zipcode matches the search
+     */
     private boolean compareZipCode(String searchQuery, Customer customer){
         return customer.getZipcode().toLowerCase().contains(searchQuery.toLowerCase());
     }
 
-        private boolean compareStreetName(String searchQuery, Customer customer){
+    /**
+     * A method that takes in a search query and a customer, and returns a boolean value indicating whether street name matches the search
+     */
+    private boolean compareStreetName(String searchQuery, Customer customer){
         return customer.getStreetName().toLowerCase().contains(searchQuery.toLowerCase());
+    }
+
+    /**
+     * A method that takes in a search query and a customer, and returns a boolean value indicating whether city matches the search
+     */
+    private boolean compareCity(String searchQuery, Customer customer){
+        return customer.getCity().toLowerCase().contains(searchQuery.toLowerCase());
     }
 }
