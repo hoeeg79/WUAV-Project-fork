ac00193cd0b1ac1abccf6026cf0e63481189c544
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-05-22 10:16:07 +0200

    Sidste kommentarer

--- src/GUI/Model/CustomerModel.java
+++ src/GUI/Model/CustomerModel.java
@@ -14,11 +14,17 @@ public class CustomerModel {
     private final CustomerManager customerManager;
     private final ObservableList<Customer> customerList;
 
+    /**
+     * A constructor of the CustomerModel. Used to instantiate CustomerManager and declare what customerList is.
+     */
     public CustomerModel() throws Exception {
         customerManager = new CustomerManager();
         customerList = FXCollections.observableArrayList();
     }
 
+    /**
+     * A method used to Create a new customer if the specified information does not exist.
+     */
     public boolean createCustomer(Customer customer) throws SQLException {
         Customer newCustomer = customerManager.createCustomer(customer);
         if (newCustomer == null) {
@@ -30,26 +36,41 @@ public class CustomerModel {
         }
     }
 
+    /**
+     * A method used to delete a customer.
+     */
     public void deleteCustomer(Customer customer) throws SQLException {
         customerManager.deleteCustomer(customer);
     }
 
+    /**
+     * A method used to update a customer.
+     */
     public void updateCustomer(Customer c) throws SQLException {
         customerManager.updateCustomer(c);
     }
 
+    /**
+     * A method that clears a list of customers, and adds them back using information from the customerManager.
+     */
     public ObservableList<Customer> getObservableCustomers() throws Exception{
         customerList.clear();
         customerList.addAll(customerManager.getCustomers());
         return customerList;
     }
 
+    /**
+     * A method used to search for customers.
+     */
     public void searchCustomer(String query) throws Exception{
         List<Customer> searchResults = customerManager.searchCustomer(query);
         customerList.clear();
         customerList.addAll(searchResults);
     }
 
+    /**
+     * A method used to check if there is a document ready for approval for a specific customer.
+     */
     public boolean checkCustomerForDocs(){
 
         for (Customer customer : customerList) {
--- src/GUI/Model/UsersModel.java
+++ src/GUI/Model/UsersModel.java
@@ -20,16 +20,25 @@ public class UsersModel {
         getObservableUsers();
     }
 
+    /**
+     *  A method used to create a new user, and add them to the userList.
+     */
     public void createUser(User user) throws SQLException {
         User newUser = usersManager.createUser(user);
         userList.add(newUser);
     }
 
+    /**
+     *  A method used delete a user and remove them frmo the userList.
+     */
     public void deleteUser(User user) throws SQLException {
         usersManager.deleteUser(user);
         userList.remove(user);
     }
 
+    /**
+     *  A method used retrieve a list of users and return the userList.
+     */
     public ObservableList<User> getObservableUsers() throws Exception {
         userList = FXCollections.observableArrayList();
         userList.addAll(usersManager.getUsers());
@@ -37,12 +46,18 @@ public class UsersModel {
         return userList;
     }
 
+    /**
+     *  A method used update users table, and update the userList.
+     */
     public void updateUser(User user) throws Exception {
         usersManager.updateUser(user);
         userList.clear();
         userList.addAll(usersManager.getUsers());
     }
 
+    /**
+     *  A method used retrieve a list of the users, and see which tech documents they are associated with.
+     */
     public List<User> getLinkedUsers(TechDoc techDoc) throws SQLException {
         return usersManager.getLinkedUsers(techDoc);
     }
